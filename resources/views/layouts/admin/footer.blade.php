6cef9970564dca6edb1fa6171a58f2731f57666d
j2cey <jud10parfait@gmail.com>
2020-07-07 20:03:18 +0000

    Intégration AdminLTE

---
+++ resources/views/layouts/admin/footer.blade.php
@@ -0,0 +1,5 @@
+<strong>Copyright © 2019-2020 Admin-IT.</strong>
+All rights reserved.
+<div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 3.0.2
+</div>
---
+++ resources/views/layouts/admin/right-sidebar.blade.php
@@ -0,0 +1,8 @@
+<section class="content">
+    <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row content-header"> Quick Settings Here... </div>
+        <div class="row"></div>
+        <!-- /.row (main row) -->
+    </div><!-- /.container-fluid -->
+</section>
